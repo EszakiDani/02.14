a2208b46e99c9a19c05ff54efe23fcd623847e9f
lenfear23 <papp.david@petrik.hu>
2023-02-14 08:25:16 +0100

    kezd

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,6 +3,7 @@ import { DataSource } from 'typeorm';
 import { AppService } from './app.service';
 
 @Controller()
+
 export class AppController {
   constructor(
     private readonly appService: AppService,
